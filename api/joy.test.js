44a7b973d0cce09596b2e2b5d8ba709bbde38b24
Katy Kane <katytheartist@gmail.com>
2024-06-06 11:25:20 -0500

    beforeall

--- api/joy.test.js
+++ api/joy.test.js
@@ -1,1 +1,15 @@
-const 
+const request = require('supertest')
+const db = require('../data/db-config')
+const server = require('../server')
+
+const joy1 = {joy_action: 'Pet a cat', joy_feeling: 'Feel a sense of inner peace and serenity'}
+const joy2 = {joy_action: 'Play frisbee with a friend', joy_feeling: 'Chill and happy'}
+
+beforeAll(async ()=>{
+    await db.migrate.rollback()
+    await db.migrate.latest()
+})
+
+    it('correct env var', ()=>{
+        expect(process.env.DB_ENV).toBe('testing')
+    })
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const server = express()
-const joyRouter = require('./api/joyRouter')
+const joyRouter = require('./api/joy-router')
 
 server.use(express.json())
 server.use('/joy', joyRouter)
 
 module.exports = server
+
+
+//    "@eslint/js": "^9.4.0",     "globals": "^15.3.0",
